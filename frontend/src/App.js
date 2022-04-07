dc74b63f16dc635d71a3a6769de743813e052545
Mustafa Khaled <moustafa.khaled@bypa-ss.com>
2022-04-07 03:15:18 +0200

    develop product Detail & cart item

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import ProductDetails from './components/product/ProductDetails'
+import Cart from './components/cart/Cart'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} exact />
           <Route path="/product/:id" element={<ProductDetails/>} exact />
+            <Route path="/cart" component={Cart} exact />
 
         </Routes>
       </div>
--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const Header = () => {
+    const { cartItems } = useSelector(state => state.cart)
+
     return (
         <Fragment>
             <nav className='navbar row'>
@@ -25,7 +29,7 @@ const Header = () => {
                 <div className='col-12 col-md-3 mt-4 mt-md-0 text-center'>
                     <button className='btn' id="login_btn" >Login</button>
                     <span id='cart' className='ml-3'>Cart</span>
-                    <span className='ml-1' id='cart_count'>2</span>
+                    <span className="ml-1" id="cart_count">{cartItems.length}</span>
                 </div>
             </nav>
 
