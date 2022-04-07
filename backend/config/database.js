07be2af58ef6c7815abedc7c27cc2b48472ec226
Eid Azzam <eidmahmoud134@gmail.com>
2022-04-07 22:58:50 +0200

    last updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,9 +5,12 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        
     }).then(con => {
         console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
     })
 }
 
 module.exports = connectDatabase
+
+
