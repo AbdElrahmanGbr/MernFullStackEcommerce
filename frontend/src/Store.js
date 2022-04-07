c46e988bcb7bda1319e88c26386787a4625e9a44
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-07 12:50:53 +0200

    authintication frontend

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -2,12 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { productsReducer,productDetailsReducer  } from "./reducers/ProductReducer"
+import {authReducer, userReducer } from "./reducers/userReducer"
 const reducer = combineReducers({
     products: productsReducer,
-    productDetails: productDetailsReducer
-
+    productDetails: productDetailsReducer,
+    auth: authReducer,
+    user: userReducer
 })
 let initialState = {}
 const middleware = [thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
-export default store
+const Store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+export default Store
